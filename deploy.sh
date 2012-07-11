8ab6de44b71e0bcfa0dbc0a7f5da2b79f70a72d6
Zdenek <vrablik@gmail.com>
2012-07-11 15:18:47 +0200

    directory and file names changes

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,19 @@ TOMCAT_PATH=~/ismart/ismart$SUFFIX
 echo "devPath: $DEV_PATH"
 echo "tomcatPath: $TOMCAT_PATH"
 
-cp $DEV_PATH/ispnSM/build/libs/ispnSM-prototype.jar $TOMCAT_PATH/lib/ispnSM.jar
-cp $DEV_PATH/tomcatJBossTM/build/libs/tomcatJBossTM-prototype.jar $TOMCAT_PATH/lib/jbossTM/tomcatJbossTM.jar
-cp $DEV_PATH/memoryAgent/build/libs/memoryAgent-prototype.jar $TOMCAT_PATH/lib/memoryAgent.jar
-cp $DEV_PATH/testInfinispanCache/build/libs/testInfinispanCache-prototype.jar $TOMCAT_PATH/lib/testInfinispanCache.jar
-cp $DEV_PATH/testDbOracle/build/libs/testDbOracle-prototype.jar $TOMCAT_PATH/lib/testDbOracle.jar
+cp $DEV_PATH/tomcatDistributedSessionManager/build/libs/tomcatDistributedSessionManager-1.0-SNAPSHOT.jar $TOMCAT_PATH/lib/tomcatDistributedSessionManager.jar
+#don't use transaction manager yet
+#cp $DEV_PATH/tomcatJBossTM/build/libs/tomcatJBossTM-.jar $TOMCAT_PATH/lib/jbossTM/tomcatJbossTM.jar
+cp $DEV_PATH/memoryAgent/build/libs/memoryAgent-1.0-SNAPSHOT.jar $TOMCAT_PATH/lib/memoryAgent.jar
+cp $DEV_PATH/testInfinispanCache/build/libs/testInfinispanCache-1.0-SNAPSHOT.jar $TOMCAT_PATH/lib/testInfinispanCache.jar
+cp $DEV_PATH/testDbOracle/build/libs/testDbOracle-1.0-SNAPSHOT.jar $TOMCAT_PATH/lib/testDbOracle.jar
 
 echo "deploy test app"
 echo "remove old"
 rm -rf $TOMCAT_PATH/webapps/testLB
 rm -rf $TOMCAT_PATH/webapps/testLB2
 echo "deploy"
-cp $DEV_PATH/testLB/build/libs/testLB-prototype.war $TOMCAT_PATH/webapps/testLB.war
-cp $DEV_PATH/testLB/build/libs/testLB-prototype.war $TOMCAT_PATH/webapps/testLB2.war
+cp $DEV_PATH/testLB/build/libs/testLB-1.0-SNAPSHOT.war $TOMCAT_PATH/webapps/testLB.war
+cp $DEV_PATH/testLB/build/libs/testLB-1.0-SNAPSHOT.war $TOMCAT_PATH/webapps/testLB2.war
 sleep 5
 ls -la $TOMCAT_PATH/webapps/ |grep testLB
